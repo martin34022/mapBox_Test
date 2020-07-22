4e30a7e410e4ba23705eee8afe361f5a862b0197
martin34022 <martin34022@gmail.com>
2020-07-22 17:58:01 +0800

    移除offline相關函式

--- app/src/main/java/com/example/mapbox_test/MainActivity.java
+++ app/src/main/java/com/example/mapbox_test/MainActivity.java
@@ -6,17 +6,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.mapbox.mapboxsdk.Mapbox;
-import com.mapbox.mapboxsdk.geometry.LatLng;
-import com.mapbox.mapboxsdk.geometry.LatLngBounds;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
-import com.mapbox.mapboxsdk.offline.OfflineManager;
-import com.mapbox.mapboxsdk.offline.OfflineTilePyramidRegionDefinition;
-import com.mapbox.mapboxsdk.plugins.offline.model.NotificationOptions;
-import com.mapbox.mapboxsdk.plugins.offline.model.OfflineDownloadOptions;
-import com.mapbox.mapboxsdk.plugins.offline.offline.OfflinePlugin;
-import com.mapbox.mapboxsdk.plugins.offline.utils.OfflineUtils;
 
 public class MainActivity extends AppCompatActivity {
     private MapView mapView;
@@ -38,34 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         });
-        OfflineManager offlineManager = OfflineManager.getInstance(MainActivity.this);
-        // Create a bounding box for the offline region
-        LatLngBounds latLngBounds = new LatLngBounds.Builder()
-                .include(new LatLng(37.7897, -119.5073)) // Northeast
-                .include(new LatLng(37.6744, -119.6815)) // Southwest
-                .build();
 
-        // Define the offline region
-        OfflineTilePyramidRegionDefinition definition = new OfflineTilePyramidRegionDefinition(
-                mapboxMap.getStyleUrl(),
-                latLngBounds,
-                10,
-                20,
-                MainActivity.this.getResources().getDisplayMetrics().density);
-
-
-        NotificationOptions notificationOptions = NotificationOptions.builder(this)
-                .smallIconRes(R.drawable.mapbox_logo_icon)
-                .returnActivity(MainActivity.class.getName())
-                .build();
-
-        OfflinePlugin.getInstance(this).startDownload(
-                OfflineDownloadOptions.builder()
-                        .definition(definition)
-                        .metadata(OfflineUtils.convertRegionName(regionName))
-                        .notificationOptions(notificationOptions)
-                        .build()
-        );
     }
     @Override
     public void onResume(){
